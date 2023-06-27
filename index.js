bfa14ce54cf7f07a35e23a42974162e52e21096f
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-06-27 13:36:26 -0500

    1 file modified

--- index.js
+++ index.js
@@ -77,3 +77,65 @@ run().catch(console.dir);
 // );
 
 // connection.end();
+
+//////////////////////////////////////
+/**
+ * Node.js added support for ES6 import and export syntax in version 13.2.0. However, this feature is behind the `--experimental-modules` flag in Node.js versions 10 and 12. In Node.js version 14 and onwards, it is no longer experimental and you can use it without a flag. 
+
+One important thing to note is that Node.js treats JavaScript files as CommonJS modules by default. To tell Node.js to treat your files as ES6 modules, you can do one of the following:
+
+1. Add `"type": "module"` to your `package.json` file. This will treat all `.js` files in your project as ES6 modules.
+
+```json
+{
+  "name": "your-project-name",
+  "version": "1.0.0",
+  "type": "module"
+}
+```
+[Source 5](https://nodejs.org/api/esm.html#esm_package_json_type_field)
+
+2. Use the `.mjs` extension for your files. Node.js will treat any file with a `.mjs` extension as an ES6 module.
+
+```js
+// lib.mjs
+export function square(x) {
+    return x * x;
+}
+
+// main.mjs
+import { square } from './lib.mjs';
+console.log(square(11)); // 121
+```
+[Source 5](https://nodejs.org/api/esm.html#esm_enabling)
+
+3. Use the `.cjs` extension for files you want to be treated as CommonJS. This is useful if you have `"type": "module"` in your `package.json`, but still have some files that you want to be treated as CommonJS.
+
+```js
+// lib.cjs
+module.exports = function square(x) {
+    return x * x;
+}
+
+// main.mjs
+import square from './lib.cjs';
+console.log(square(11)); // 121
+```
+[Source 5](https://nodejs.org/api/esm.html#esm_enabling)
+
+Please note that the ES6 `import` statement only supports importing default exports in Node.js. To import named exports, you have to use a workaround:
+
+```js
+// lib.mjs
+export function square(x) {
+    return x * x;
+}
+
+// main.mjs
+import * as lib from './lib.mjs';
+console.log(lib.square(11)); // 121
+```
+[Source 5](https://nodejs.org/api/esm.html#esm_import_statements)
+
+With these changes, you should be able to use ES6 import and export in your Node.js project.
+ */
